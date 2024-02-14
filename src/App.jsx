fda1b3c0cea9c39fbf2dba19cda766bc523e2d4c
Apichet Juntodum <Apichet.jtd@outlook.com>
2024-02-14 15:00:20 +0700

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useTheme } from "../src/Components/common/Theme/ThemeContext/ThemeContext"; // Import the custom hook from ThemeContex
 // import { Box } from "@mui/material";
 import AppBarComponent from "./Components/common/Appbar/AppBarComponent";
 import DrawerComponent from "./Components/common/Appbar/Drawer_mini/DrawerComponent";
 import MainContent from "./Components/common/Appbar/Drawer_mini/MainContent";
+import useMediaQuery from "@mui/material/useMediaQuery";
 // Drawer_Persistent;
 // Drawer_mini;
 function App() {
   const { isDarkMode } = useTheme(); // Use the isDarkMode function from Context
   const [open, setOpen] = useState(false);
+  const matches = useMediaQuery("(min-width:600px)");
   const handleDrawerOpen = () => {
     setOpen(true);
   };
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  useEffect(() => {
+    if (matches) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }, [matches]);
 
   return (
     <>
@@ -25,6 +35,7 @@ function App() {
           handleDrawerOpen={handleDrawerOpen}
           isDarkMode={isDarkMode}
         />
+
         <DrawerComponent
           open={open}
           handleDrawerClose={handleDrawerClose}
